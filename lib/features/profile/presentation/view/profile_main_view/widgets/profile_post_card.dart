86b9fb299b3dd165648c660834cecb08c727b323
EFIIIIMOV <94187053+EFIIIIMOV@users.noreply.github.com>
2024-06-04 22:34:39 +0300

    add/get profile post server work

--- lib/features/profile/presentation/view/profile_main_view/widgets/profile_post_card.dart
+++ lib/features/profile/presentation/view/profile_main_view/widgets/profile_post_card.dart
@@ -15,6 +15,49 @@ class ProfilePostCard extends StatelessWidget {
     required this.postEntity,
   });
 
+  void _showImageOnFullScreen(BuildContext context, String imagePath) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.white,
+          elevation: 1,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Container(
+            height: 500,
+            width: 400,
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: Container(
+                  color: Colors.black,
+                  child: Image.network(
+                    imagePath,
+                    fit: BoxFit.fill,
+                    frameBuilder: (BuildContext context, Widget child,
+                        int? frame, bool wasSynchronouslyLoaded) {
+                      return child;
+                    },
+                    loadingBuilder: (BuildContext context, Widget child,
+                        ImageChunkEvent? loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return CircularProgressIndicator();
+                    },
+                    errorBuilder: (context, error, stackTrace) {
+                      return Text('Ошибка загрузки изображения');
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -35,29 +78,33 @@ class ProfilePostCard extends StatelessWidget {
       ),
     );
   }
-}
 
-List<Widget> generateList(BuildContext context, List<String> imageIds) {
-  return [
-    GridView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: imageIds.length,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 5,
-        crossAxisSpacing: 8.0,
-        mainAxisSpacing: 8.0,
+  List<Widget> generateList(BuildContext context, List<String> imageIds) {
+    return [
+      GridView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: imageIds.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 5,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () => _showImageOnFullScreen(context,
+                'http://10.0.2.2:8000/user/profile/post/image/?image_id=${imageIds[index].replaceAll('-', '')}'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: CreateImageWidget(
+                  borderRadiusCircular: 8,
+                  containerSize: 50,
+                  imageUrl:
+                      'http://10.0.2.2:8000/user/profile/post/image/?image_id=${imageIds[index].replaceAll('-', '')}'),
+            ),
+          );
+        },
       ),
-      itemBuilder: (BuildContext context, int index) {
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(8.0),
-          child: CreateImageWidget(
-              borderRadiusCircular: 8,
-              containerSize: 50,
-              imageUrl:
-                  'http://10.0.2.2:8000/user/profile/post/image/?image_id=${imageIds[index].replaceAll('-', '')}'),
-        );
-      },
-    ),
-  ];
+    ];
+  }
 }
